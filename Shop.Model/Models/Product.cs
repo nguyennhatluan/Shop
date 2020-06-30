fafe8f24fb458bd7a2083ef7d1ba0108fba77044
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-01 01:30:17 +0700

    Add tables in Model part 2

---
+++ Shop.Model/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Shop.Model.Models
+{
+    [Table("Products")]
+    public class Product
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public string Alias { set; get; }
+        public int CategoryID { set; get; }
+        public string Image { set; get; }
+        public XElement MoreImage { set; get; }
+        public decimal Price { set; get; }
+        public decimal? PromotionPrice { set; get; }
+        public int Warranty { set; get; }
+        public string Description { set; get; }
+        public string Content { set; get; }
+        public bool? HomeFlag { set; get; }
+        public bool? HotFlag { set; get; }
+        public int? ViewCount { set; get; }
+    }
+}
---
+++ Shop.Model/Models/ProductCategory.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Model.Abstract;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Model.Models
+{
+    [Table("ProductCategories")]
+    public class ProductCategory:Auditable
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+        public string Alias { set; get; }
+        public string Description { set; get; }
+        public int? ParentID { set; get; }
+        public int? DisplayOrder { set; get; }
+        public string Image { set; get; }
+        public bool? HomeFlag { set; get; }
+    }
+}
