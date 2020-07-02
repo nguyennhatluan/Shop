27f0c1a751547549b9a84a435b8954f1b011dba0
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-03 01:01:17 +0700

    add class extend from repository class

---
+++ Shop.Data/Repositories/PostCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using Shop.Data.Infrastructure;
+using Shop.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Data.Repositories
+{
+    public class ProductCategoryRepository : RepositoryBase<ProductCategory>
+    {
+        // phải khai báo lại constructor trong lớp cha
+        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        {
+        }
+    }
+}
