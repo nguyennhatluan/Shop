f0e5b99657ef32b89b577c95de1759feb77fe030
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-04 13:21:49 +0700

    aaa

--- Shop.Model/Models/Post.cs
+++ Shop.Model/Models/Post.cs
@@ -34,7 +34,6 @@ namespace Shop.Model.Models
         public int? ViewCount { set; get; }
         [ForeignKey("CategoryID")]
         public virtual PostCategory PostCategory { set; get; }
-
        // public virtual IEnumerable<PostTag> PostTags { set; get; }
     }
 }
