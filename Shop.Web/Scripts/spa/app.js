cbdbab9d3f25a64a2baed213ceb55a5327956975
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-13 08:37:42 +0700

    aaa

---
+++ Shop.Web/Scripts/spa/app.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../plugins/angular.min.js" />
+var myApp = angular.module("myModule", []);
+myApp.controller("myController", myController);
+myController.$inject = ['$scope'];
+function myController($scope) {
+    $scope.message = "This is my message from controller";
+    $scope.name = "Nguyễn Nhất Luân";
+}
