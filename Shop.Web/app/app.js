baea8c5a4b7cf015cc44bd8df8fb36d329d06a36
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-14 09:41:39 +0700

    angular ui route

---
+++ Shop.Web/app/app.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../scripts/plugins/angular.js" />
+
+
+
+
+(function () {
+    angular.module('shop', ['shop.products', 'shop.common']).config(config);
+
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('home', {
+            url: "/admin",
+            templateUrl: "/app/components/home/homeView.html",
+            controller: "homeController"
+        });
+        $urlRouterProvider.otherwise('/admin');
+    }
+})();
