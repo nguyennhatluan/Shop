f44c02e974b0be13205d1eea6e0b43373e645289
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-16 16:42:37 +0700

    Add pagination

--- Shop.Web/Api/ProductCategoryController.cs
+++ Shop.Web/Api/ProductCategoryController.cs
@@ -3,7 +3,9 @@ using Shop.Model.Models;
 using Shop.Service;
 using Shop.Web.Infrastructure.Core;
 using Shop.Web.Models;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -19,15 +21,25 @@ namespace Shop.Web.Api
             _productCategoryService = productCategoryService;
         }
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 2)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var listProductCategory = _productCategoryService.GetAll();
+                totalRow = listProductCategory.Count();
+                var query = listProductCategory.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
                 var config = new MapperConfiguration(cfg => { cfg.CreateMap<ProductCategory, ProductCategoryViewModel>(); });
                 IMapper imapper = config.CreateMapper();
-                var listProductCategoryVm = imapper.Map<List<ProductCategoryViewModel>>(listProductCategory);
-                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, listProductCategoryVm);
+                var responseData = imapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)
+                };
+                HttpResponseMessage response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
 
                 return response;
             });
---
+++ Shop.Web/app/Shared/directive/pagerDirective.js
@@ -0,0 +1,50 @@
+﻿(function (app) {
+    'use strict';
+
+    app.directive('pagerDirective', pagerDirective);
+
+    function pagerDirective() {
+        return {
+            scope: {
+                page: '@', // @ là nhận dữ liệu 1 chiều từ scope của controller đến scope của directive
+                pagesCount: '@',
+                totalCount: '@',
+                searchFunc: '&',
+                customPath: '@' // & nhận các method 1 chiều từ scope của controller đến scope của directive
+            },
+            replace: true, //replace: true có nghĩa là nội dung của mẫu lệnh sẽ thay thế phần tử mà lệnh được khai báo
+            restrict: 'E',
+            templateUrl: '/app/shared/directive/pagerDirective.html',
+            controller: [
+                '$scope', function ($scope) {
+                    $scope.search = function (i) {
+                        if ($scope.searchFunc) {
+                            $scope.searchFunc({ page: i });
+                        }
+                    };
+
+                    $scope.range = function () {
+                        if (!$scope.pagesCount) { return []; }
+                        var step = 2;
+                        var doubleStep = step * 2;
+                        var start = Math.max(0, $scope.page - step);
+                        var end = start + 1 + doubleStep;
+                        if (end > $scope.pagesCount) { end = $scope.pagesCount; }
+
+                        var ret = [];
+                        for (var i = start; i != end; ++i) {
+                            ret.push(i);
+                        }
+
+                        return ret;
+                    };
+
+                    $scope.pagePlus = function (count) {
+                        return +$scope.page + count;
+                    }
+
+                }]
+        }
+    }
+
+})(angular.module('shop.common'));
