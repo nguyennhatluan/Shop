a1887b28024232108e48e07488375fdcb71f8838
NguyenNhatLuan <nhatluan98@gmail.com>
2020-07-30 14:19:54 +0700

    Add login form

--- Shop.Web/app/components/products/products.module.js
+++ Shop.Web/app/components/products/products.module.js
@@ -8,14 +8,17 @@
     function config($stateProvider) {
         $stateProvider.state('products', {
             url: '/products',
+            parent:'base',
             templateUrl: '/app/components/products/productListView.html',
             controller: 'productListController'
         }).state('products_add', {
             url: '/products_add',
+            parent: 'base',
             templateUrl: '/app/components/products/productAddView.html',
             controller: 'productAddController'
         }).state('products_edit', {
             url: '/products_edit/:id',
+            parent: 'base',
             templateUrl: '/app/components/products/productEditView.html',
             controller: 'productEditController'
         });
