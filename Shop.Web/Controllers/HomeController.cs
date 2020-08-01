e1919e0a3e15c667fd82d7b4ca9e4fecfbf8e2ca
NguyenNhatLuan <nhatluan98@gmail.com>
2020-08-01 10:18:14 +0700

    add partialView

--- Shop.Web/Controllers/HomeController.cs
+++ Shop.Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using Shop.Model.Models;
+using Shop.Service;
+using Shop.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +12,13 @@ namespace Shop.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IProductCategoryService _productCategoryService;
+
+        public HomeController(IProductCategoryService productCategoryService)
+        {
+            _productCategoryService = productCategoryService;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -20,7 +31,12 @@ namespace Shop.Web.Controllers
         [ChildActionOnly]
         public ActionResult Category()
         {
-            return PartialView();
+            var listProductCategories = _productCategoryService.GetAll();
+            var config = new MapperConfiguration(cfg => { cfg.CreateMap<ProductCategory, ProductCategoryViewModel>(); });
+            IMapper imapper = config.CreateMapper();
+            var listProductCategoriesViewModel = imapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(listProductCategories);
+            
+            return PartialView(listProductCategoriesViewModel);
         }
 
         [ChildActionOnly]
